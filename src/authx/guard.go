81d2d898f884231c4927a624efbcd01fb624655f
dev-nick <natchalit.nam@dohome.co.th>
2023-07-27 12:49:00 +0700

    auth

--- src/authx/guard.go
+++ src/authx/guard.go
@@ -1,18 +1,23 @@
 package authx
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Guard(c *gin.Context) error {
-	s := c.Request.Header.Get("Authorization")
-	token := strings.TrimPrefix(s, "Bearer ")
+func Guard() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		s := c.Request.Header.Get("Authorization")
+		token := strings.TrimPrefix(s, "Bearer ")
 
-	if ex := ValidEmptyToken(token); ex != nil {
-		return ex
+		if ex := ValidEmptyToken(token); ex != nil {
+			c.JSON(http.StatusUnauthorized, "Invalid Token(41)")
+			c.Abort()
+			return
+		}
+
+		c.Next()
 	}
-
-	return nil
 }
