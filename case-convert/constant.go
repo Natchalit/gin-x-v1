b7309222a5029fcb99c5bbc74c1e15f4a98e2e20
dev-nick <natchalit.nam@dohome.co.th>
2023-08-02 17:56:15 +0700

    case-convert

---
+++ case-convert/constant.go
@@ -0,0 +1,31 @@
+package caseconvert
+
+import (
+	"regexp"
+	"strings"
+)
+
+func ToConstant(input string) string {
+	// Convert to CONSTANT_CASE using regex
+	re := regexp.MustCompile(`[a-zA-Z]`)
+	constantCaseStr := re.ReplaceAllStringFunc(input, func(s string) string {
+		return strings.ToUpper(s)
+	})
+	return constantCaseStr
+}
+
+func PascalToConstant(input string) string {
+	re := regexp.MustCompile(`([a-z0-9])([A-Z])`)
+	constantCaseStr := re.ReplaceAllString(input, "${1}_${2}")
+	return strings.ToUpper(constantCaseStr)
+}
+
+func CamelToConstant(input string) string {
+	re := regexp.MustCompile(`([a-z0-9])([A-Z])`)
+	constantCaseStr := re.ReplaceAllString(input, "${1}_${2}")
+	return strings.ToUpper(constantCaseStr)
+}
+
+func SnakeToConstant(input string) string {
+	return strings.ToUpper(input)
+}
