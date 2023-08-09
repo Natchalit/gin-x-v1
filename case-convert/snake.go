ba926e3025ee635cded58bd7bc9621e16b0f7132
dev-nick <natchalit.nam@dohome.co.th>
2023-08-09 12:51:52 +0700

    snake

--- case-convert/snake.go
+++ case-convert/snake.go
@@ -5,13 +5,14 @@ import (
 	"strings"
 )
 
-var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-
 func ToSnake(input string) string {
-	snake := matchFirstCap.ReplaceAllString(input, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-	return strings.ToLower(snake)
+	// Convert to snake_case using regex
+	re := regexp.MustCompile(`[A-Z]`)
+	input = strings.ReplaceAll(input, ` `, ``)
+	snakeCaseStr := re.ReplaceAllStringFunc(input, func(s string) string {
+		return "_" + strings.ToLower(s)
+	})
+	return strings.TrimLeft(snakeCaseStr, "_")
 }
 
 func PascalToSnake(input string) string {
