f7d49c40a4a4f8aafdb8a65ab1b88c24e46bee22
dev-nick <natchalit.nam@dohome.co.th>
2023-08-09 14:22:23 +0700

    clean connection

--- mainx/clean.go
+++ mainx/clean.go
@@ -2,11 +2,13 @@ package mainx
 
 import (
 	"sync"
+
+	"github.com/Natchalit/gin-x-v1/sqlx"
 )
 
 func Clean() {
 	var wg sync.WaitGroup
 	wg.Add(4)
-	// go func() { defer wg.Done(); sqlx.Cleanups() }()
+	go func() { defer wg.Done(); sqlx.CleanConnections() }()
 	wg.Wait()
 }
