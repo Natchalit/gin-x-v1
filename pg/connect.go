202b44d5cab472705c222b9fe3c5f2d7df25da7d
dev-nick <natchalit.nam@dohome.co.th>
2023-08-11 11:45:10 +0700

    save upsert

--- pg/connect.go
+++ pg/connect.go
@@ -1,13 +1,15 @@
 package pg
 
-import "github.com/Natchalit/gin-x-v1/connections"
+import (
+	"github.com/Natchalit/gin-x-v1/connections"
+)
 
 func (c *Connect) _Connect() error {
 	db, ex := connections.ConPg(c.DBName)
 	if ex != nil {
 		return ex
 	}
-
-	c.db.Db = db.Db
+	dbx := db.Db
+	c.db.Db = dbx
 	return nil
 }
